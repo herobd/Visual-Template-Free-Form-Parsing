aec8e63b3dd7920be4b8e2eb6d69997af4671769
Brian Davis <herobd@gmail.com>
2018-10-23 14:09:14 -0600

    remove 501r stuff

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from model.unet import UNet
-from model.my_unet import MyUnet
+#from model.my_unet import MyUnet
 from model.unet_dilated import UNetDilated
 from model.sol_eol_finder import SOL_EOL_Finder
 from model.detector import Detector

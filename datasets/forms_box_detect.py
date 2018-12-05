Fixed(?) rotation stuff so data is consistant with loss things.

datasets/forms_box_detect.py
@@ -99,6 +99,7 @@ def collate(batch):
     largest_point_label = {}
     bb_sizes=[]
     bb_dim=None
+    line_dim=None
     for b in batch:
         if b is None:
             continue
@@ -119,6 +120,7 @@ def collate(batch):
                 line_label_sizes[name].append(0)
             else:
                 line_label_sizes[name].append(gt.size(1)) 
+                line_dim = gt.size(2)
         for name,gt in b['point_gt'].items():
             if gt is None:
                 point_label_sizes[name].append(0)
@@ -178,7 +180,7 @@ def collate(batch):
     line_labels = {}
     for name,count in largest_line_label.items():
         if count != 0:
-            line_labels[name] = torch.zeros(batch_size, count, 2)
+            line_labels[name] = torch.zeros(batch_size, count, line_dim)
         else:
             line_labels[name]=None
     for i, b in enumerate(batch):
@@ -479,6 +481,8 @@ class FormsBoxDetect(torch.utils.data.Dataset):
             else:
                 table_points=None
             pixel_gt = out['pixel_gt']
+            start_of_line = out['line_gt']['start_of_line']
+            end_of_line = out['line_gt']['end_of_line']
 
             ##tic=timeit.default_timer()
             if np_img.shape[2]==3:

utils/crop_transform.py
@@ -366,6 +366,7 @@ class CropBoxTransform(object):
         pixel_gt = sample['pixel_gt'] if 'pixel_gt' in sample else None
         query_bb = sample['query_bb'] if 'query_bb' in sample else None
 
+        #rotation
         if self.rotate:
             amount = np.random.normal(0,self.degree_std_dev)
             M = cv2.getRotationMatrix2D((org_img.shape[1]/2,org_img.shape[0]/2),amount,1)
@@ -377,12 +378,20 @@ class CropBoxTransform(object):
                 pixel_gt = cv2.warpAffine(pixel_gt,M,(pixel_gt.shape[1],pixel_gt.shape[0]))
                 if len(pixel_gt.shape)==2:
                     pixel_gt = pixel_gt[:,:,None]
-            #rotate pointsa
+            #rotate points
             if bb_gt is not None:
                 points = np.reshape(bb_gt[0,:,0:16],(-1,2)) #reshape all box points to vector of x,y pairs
                 points = np.append(points,np.ones((points.shape[0],1)),axis=1) #append 1 to make homogeneous (x,y,1)
                 points = M.dot(points.T).T #multiply rot matrix
                 bb_gt[0,:,0:16] = np.reshape(points,(-1,16)) #reshape back to single vector for each bb
+
+            if line_gts is not None:
+                for name,gt in line_gts.items():
+                    if gt is not None:
+                        points = np.reshape(gt[0,:,0:4],(-1,2)) #reshape all line points to vector of x,y pairs
+                        points = np.append(points,np.ones((points.shape[0],1)),axis=1) #append 1 to make homogeneous (x,y,1)
+                        points = M.dot(points.T).T #multiply rot matrix
+                        gt[0,:,0:4] = np.reshape(points,(-1,4)) #reshape back to single vector for each line
     
             if point_gts is not None:
                 for name,gt in point_gts.items():
@@ -466,6 +475,8 @@ class CropBoxTransform(object):
                 if gt is not None:
                     gt[:,:,0] = gt[:,:,0] + pad_params[1][0]
                     gt[:,:,1] = gt[:,:,1] + pad_params[0][0]
+                    gt[:,:,2] = gt[:,:,2] + pad_params[1][0]
+                    gt[:,:,3] = gt[:,:,3] + pad_params[0][0]
 
 
         crop_params, org_img, pixel_gt, line_gt_match, point_gt_match, new_bb_gt , cropPoint = generate_random_crop(org_img, pixel_gt, line_gts, point_gts, self.random_crop_params, bb_gt=bb_gt, query_bb=query_bb, cropPoint=cropPoint)
@@ -499,6 +510,8 @@ class CropBoxTransform(object):
                     gt = gt[line_gt_match[name]][None,...] #add batch dim (?)
                     gt[...,0] = gt[...,0] - crop_params['dim1'][0]
                     gt[...,1] = gt[...,1] - crop_params['dim0'][0]
+                    gt[...,2] = gt[...,2] - crop_params['dim1'][0]
+                    gt[...,3] = gt[...,3] - crop_params['dim0'][0]
                     new_line_gts[name]=gt
         ##print('pad-minus: {}'.format(timeit.default_timer()-##tic))
 

utils/forms_annotations.py
@@ -63,7 +63,7 @@ def convertBBs(bbs,rotate,numClasses):
 
     cX = (lX+rX)/2.0
     cY = (lY+rY)/2.0
-    rot = np.arctan2((rY-lY),rX-lX)
+    rot = np.arctan2(-(rY-lY),rX-lX)
     height = np.abs(h)    #this is half height
     width = d/2.0 #and half width
 
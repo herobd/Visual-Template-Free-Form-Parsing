bfc1f0a7941a4f4a8969567439baaa19afa771d3
Brian Davis <herobd@gmail.com>
2019-02-01 16:21:36 -0700

    This has work on graph Transformer. also new cf

--- model/simpleNN.py
+++ model/simpleNN.py
@@ -14,19 +14,32 @@ class SimpleNN(BaseModel):
         hiddenSize = config['hidden_size'] if 'hidden_size' in config else 1024
         outSize = config['out_size'] if 'out_size' in config else 1
 
+        reverse = config['reverse_activation'] if 'reverse_activation' in config else False #for resnet stuff
+        norm = config['norm'] if 'norm' in config else 'batch_norm'
+        dropout = float(config['dropout']) if 'dropout' in config else 0.4
+
         layers= []
         for i in range(numLayers):
             if i==0:
                 inSize=featSize
             else:
                 inSize=hiddenSize
-            layers += [
-                nn.Linear(inSize,hiddenSize),
-                nn.BatchNorm1d(hiddenSize),
-                nn.ReLU(inplace=True),
-                nn.Dropout(0.4)
-                ]
-        layers.append(nn.Linear(hiddenSize,outSize))
+
+            if not reverse:
+                layers.append(nn.Linear(inSize,hiddenSize))
+            if norm=='batch_norm':
+                layers.append(nn.BatchNorm1d(hiddenSize))
+            elif norm=='group_norm':
+                layers.append(nn.GroupNorm(getGroupSize(hiddenSize),hiddenSize))
+            if not reverse or i!=0:
+                layers += [
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(dropout)
+                    ]
+            if reverse:
+                layers.append(nn.Linear(inSize,hiddenSize))
+        if outSize>0:
+            layers.append(nn.Linear(hiddenSize,outSize))
         self.layers=nn.Sequential(*layers)
 
     def forward(self,input):
